1a618ef1a3247f920340c71de2b90b7d82c0a1b7
unlomtrois <vadim.kuz02@gmail.com>
2023-08-20 22:02:16 +0300

    change cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     credentials: true,
   });
 
@@ -34,7 +34,7 @@ async function bootstrap() {
   const PORT = configService.get<number>('API_PORT');
 
   await app.listen(PORT).then(() => {
-    console.info(`Server started on port ${PORT}`);
+    console.info(`Server started on port http://localhost:${PORT}/api`);
   });
 }
 bootstrap();
